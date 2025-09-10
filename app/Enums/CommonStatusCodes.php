86028175bceb3f0dfb9925d9860a634e1a23c855
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-09-10 11:23:06 +0530

    Enums created and updated

---
+++ app/Enums/CommonStatusCodes.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum CommonStatusCodes: int
+{
+    case BATCH = 1;
+    case NON_BATCH = 0;
+}
---
+++ app/Enums/TransactionCodes.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum TransactionCodes: int
+{
+    case ITEM_MASTER_TRANSACTION_CODE = 100;
+}
