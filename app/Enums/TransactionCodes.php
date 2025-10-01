a43049de74982c8798b3e4d57ed8c1b41018a847
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:37:06 +0530

    Update TransactionCodes.php

--- app/Enums/TransactionCodes.php
+++ app/Enums/TransactionCodes.php
@@ -5,4 +5,6 @@ namespace App\Enums;
 enum TransactionCodes: int
 {
     case ITEM_MASTER_TRANSACTION_CODE = 100;
+    case ITEM_CATEGORY_TRANSACTION_CODE = 101;
+    case ITEM_BRAND_TRANSACTION_CODE = 102;
 }
