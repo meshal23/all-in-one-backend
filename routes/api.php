8bb02cc8d54276f5dc46d5f0b975c16b2c9b284a
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:44:54 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ItemBrandController\ItemBrandController;
+use App\Http\Controllers\ItemCategoryController\ItemCategoryController;
 use App\Http\Controllers\ItemMasterController\ItemMasterController;
 use App\Http\Controllers\ProductController\ProductController;
+use App\Models\ItemMaster\ItemMaster;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +16,10 @@ Route::get('user', function (Request $request) {
     ]);
 })->middleware('auth:sanctum');
 // Route::resource('products', ProductController::class);
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('products', ProductController::class);
     Route::resource('item-master', ItemMasterController::class);
+    Route::resource('item-brand', ItemBrandController::class);
+    Route::resource('item-category', ItemCategoryController::class);
 });
